1fd48ef0aceaf5a69e5b378e9284f877b0c8348d
tomgerzabek <tom.gerzabek@icloud.com>
2025-09-25 14:32:19 +0200

    video-08: Live Search in UI5

--- ui5/Users/resources/app/controller/App.controller.js
+++ ui5/Users/resources/app/controller/App.controller.js
@@ -1,9 +1,25 @@
 sap.ui.define([
-    "com/laravelui5/core/BaseController"
-], function (BaseController) {
+    "com/laravelui5/core/BaseController",
+    "sap/ui/model/Filter",
+    "sap/ui/model/FilterOperator",
+], function (BaseController, Filter, FilterOperator) {
     "use strict";
 
     return BaseController.extend("io.pragmatiqu.users.controller.App", {
+        onSearch: function (event) {
+            const q = event.getParameter("newValue");
+            const list = this.byId("usersList");
+            const binding = list.getBinding("items");
 
+            const filters = [];
+            if (q) {
+                filters.push(new Filter({
+                    path: "name",
+                    operator: FilterOperator.Contains,
+                    value1: q
+                }));
+            }
+            binding.filter(filters);
+        }
     });
 });
